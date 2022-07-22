bc4a2a1ffb59aabed4fb91a2c42a0ea747638ae0
Harrison <harrison@phillingham.com>
2022-07-22 22:53:17 +0100

    Updated Density/Speed

--- src/BackgroundParticles.svelte
+++ src/BackgroundParticles.svelte
@@ -52,13 +52,13 @@
 
             function init() {
                 particlesArray = [];
-                let numberOfParticles = (canvas.height * canvas.width) / 6000;
+                let numberOfParticles = (canvas.height * canvas.width) / 7000;
                 for (let i = 0; i < numberOfParticles; i++) {
                     let size = Math.random() * 2 + 1;
                     let x = ((Math.random() * (canvas.width + 30)) - 15);
                     let y = ((Math.random() * (canvas.height + 30)) - 15);
-                    let speedX = ((Math.random() * size) - 0.75) * (1 / size);
-                    let speedY = ((Math.random() * size) - 0.75) * (1 / size);
+                    let speedX = ((Math.random() * size) - 0.75) * (1 / (1.5*size));
+                    let speedY = ((Math.random() * size) - 0.75) * (1 / (1.5*size));
                     particlesArray.push(new Particle(x, y, speedX, speedY, size, particleColor));
                 }
             }
